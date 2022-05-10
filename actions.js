26068daa105e7d19744934151c7eef1d33c755dd
KElee02 <ka_matsudo@yahoo.co.jp>
2022-05-10 19:23:58 +0800

    website update

--- actions.js
+++ actions.js
@@ -1,3 +1,14 @@
-function turnoff(obj) {
-    document.getElementById(obj).style.display = "none";
+let introScreen = true;
+window.addEventListener("keypress", function (event) {
+    // 当按下Enter键点击开始按钮
+    if (event.key === "Enter") {
+        document.getElementById("start-button").click();
+    }
+});
+
+function start() {
+    document.getElementById("intro-screen").style.visibility = "hidden";
+    document.getElementById("over-canvas").style.visibility = "visible";
+    document.getElementById("under-canvas").style.visibility = "visible"
+    introScreen = false;
 }
