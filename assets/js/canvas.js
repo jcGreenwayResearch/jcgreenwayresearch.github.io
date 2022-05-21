d84fd5a5e5b4b8f16418b5488717f15273b9b096
KElee02 <ka_matsudo@yahoo.co.jp>
2022-05-21 20:18:39 +0800

    website finished

--- assets/js/canvas.js
+++ assets/js/canvas.js
@@ -24,12 +24,13 @@ function init() {
         document.getElementById("start-button").style.visibility = "visible";
         document.getElementById("loading-animation").style.visibility = "hidden";
     };
-    manager.onError = function (url) {
+    manager.onError = function () {
         document.getElementById("start-button").style.visibility = "visible";
         document.getElementById("start-button").textContent = "有错误 仍然开始";
         document.getElementById("loading-animation").style.visibility = "hidden";
     };
 
+    // 加载两个球
     // 加载loader
     let loader = new THREE.STLLoader(manager);
     // 导入树干
@@ -41,7 +42,7 @@ function init() {
             });
             let trunks = new THREE.Mesh(trunkGeometry, trunkMaterial);
 
-            trunks.position.set(0, 0, 0);
+            trunks.position.set(30, 0, 0);
             trunks.rotation.set(-Math.PI / 2, 0, 0);
             trunks.scale.set(2, 2, 2);
 
@@ -61,7 +62,7 @@ function init() {
             });
             let leaves = new THREE.Mesh(leavesGeometry, leavesMaterial);
 
-            leaves.position.set(0, 0, 0);
+            leaves.position.set(30, 0, 0);
             leaves.rotation.set(-Math.PI / 2, 0, 0);
             leaves.scale.set(2, 2, 2);
 
@@ -77,7 +78,7 @@ function init() {
         'model/island.stl',
         function (island1Geometry) {
             let island1Material = new THREE.MeshStandardMaterial({
-                color: 0xFF9245,
+                color: 0x45494A,
                 metalness: 0,
                 roughness: 0.8
             });
@@ -89,40 +90,74 @@ function init() {
 
             island1.castShadow = true;
 
+            // 第二个沙盘
+            let island2Material = new THREE.MeshStandardMaterial({
+                color: 0xFF9245,
+                metalness: 0,
+                roughness: 0.8
+            });
+            let island2 = new THREE.Mesh(island1Geometry, island2Material);
+            island2.scale.set(2, 2, 1.5)
+            island2.rotateX(Math.PI / -2);
+            island2.rotateZ(Math.PI / 2)
+            island2.translateY(-30);
+
+            // 第三个沙盘
+            let island3 = island1.clone();
+            island3.translateX(60);
+
             sandboxes.add(island1);
+            sandboxes.add(island2);
+            sandboxes.add(island3);
         },
     )
 
 
     // 创建贴图
+    // 工程地图
+    let mapProjectTexture = new THREE.TextureLoader().load("img/project.png");
+    mapProjectTexture.wrapS = THREE.RepeatWrapping;
+    let mapProjectPlane = new THREE.PlaneGeometry(10, 10);
+    let mapProjectMaterial = new THREE.MeshToonMaterial({map: mapProjectTexture});
+    let mapProject = new THREE.Mesh(mapProjectPlane, mapProjectMaterial);
+
+    mapProject.rotateX(-Math.PI / 2);
+    mapProject.receiveShadow = true;
+
+    sandboxes.add(mapProject);
+
+    // 距离地图
     let mapChengduTexture = new THREE.TextureLoader().load("img/map.jpg");
     mapChengduTexture.wrapS = THREE.RepeatWrapping;
-
-
-    // 创建地图平面
     let mapChengduPlane = new THREE.PlaneGeometry(10, 10);
     let mapChengduMaterial = new THREE.MeshToonMaterial({map: mapChengduTexture});
     let mapChengdu = new THREE.Mesh(mapChengduPlane, mapChengduMaterial);
 
-    mapChengdu.rotation.x = -Math.PI / 2;
+    mapChengdu.translateX(30);
+    mapChengdu.rotateX(-Math.PI / 2);
     mapChengdu.receiveShadow = true;
 
     sandboxes.add(mapChengdu);
 
-    let cubeGeometry = new THREE.BoxGeometry(5, 5, 5);
-    let cubeMaterial = new THREE.MeshToonMaterial({color: 0x7B7BEF});
-    let cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+    // 词云
+    let wordCloudTexture = new THREE.TextureLoader().load("img/wordcloud.png");
+    wordCloudTexture.wrapS = THREE.RepeatWrapping;
+    let wordCloudPlane = new THREE.PlaneGeometry(10, 10);
+    let wordCloudMaterial = new THREE.MeshToonMaterial({map: wordCloudTexture});
+    let wordCloud = new THREE.Mesh(wordCloudPlane, wordCloudMaterial);
 
-    // 添加方块
-    cube.translateX(15);
-    cube.rotateX(Math.PI / 4);
-    cube.rotateY(Math.PI / 4);
-    cube.rotateZ(Math.PI / 4);
-    sandboxes.add(cube);
+    wordCloud.translateX(60);
+    wordCloud.rotateX(-Math.PI / 2);
+    wordCloud.receiveShadow = true;
+
+    sandboxes.add(wordCloud);
+
+    sandboxes.translateY(-3)
+
 
     // 创建聚光灯
     let spotLight = new THREE.SpotLight(0xBBBBBB);
-    spotLight.bias = 0.1
+    spotLight.bias = 0.1;
     spotLight.position.x = -10;
     spotLight.position.y = 15;
     spotLight.position.z = 18;
@@ -134,12 +169,12 @@ function init() {
     spotLight.shadow.mapSize.height = 2048;
     spotLight.angle = Math.PI / 10;
 
-    scene.add(spotLight)
+    scene.add(spotLight);
 
 
     // 创建环境光
     let ambientLight = new THREE.AmbientLight(0x393939);
-    scene.add(ambientLight)
+    scene.add(ambientLight);
 
 
     // 定位摄像机，指向场景中心
@@ -170,11 +205,17 @@ function init() {
     document.getElementById("arrow-right").addEventListener("click", () => {
         // 判断intro-screen是否关闭
         if (document.getElementById("intro-screen").style.visibility !== "hidden") {
-            return
+            return;
         }
         // 判断之前的移动动画是否结束
         if (sandboxesMoving === true) {
-            return
+            return;
+        }
+        if (cameraMoving === true) {
+            return;
+        }
+        if (isScrolling === true) {
+            return;
         }
 
         sandboxSwitcher(true);
@@ -182,11 +223,17 @@ function init() {
     document.getElementById("arrow-left").addEventListener("click", () => {
         // 判断intro-screen是否关闭
         if (document.getElementById("intro-screen").style.visibility !== "hidden") {
-            return
+            return;
         }
         // 判断之前的移动动画是否结束
         if (sandboxesMoving === true) {
-            return
+            return;
+        }
+        if (cameraMoving === true) {
+            return;
+        }
+        if (isScrolling === true) {
+            return;
         }
 
         sandboxSwitcher(false);
@@ -194,17 +241,33 @@ function init() {
     document.getElementById("arrow-up-down").addEventListener("click", () => {
         // 判断intro-screen是否关闭
         if (document.getElementById("intro-screen").style.visibility !== "hidden") {
-            return
+            return;
         }
         // 判断之前的移动动画是否结束
         if (sandboxesMoving === true) {
-            return
+            return;
+        }
+        if (cameraMoving === true) {
+            return;
+        }
+        if (isScrolling === true) {
+            return;
+        }
+
+        if (enableCameraMoving === false) {
+            if (enableScroll === true) {
+                if (scrolledUp === false) {
+                    scrollPassage(true);
+                    return;
+                } else {
+                    scrollPassage(false);
+                    return;
+                }
+            }
         }
         if (topAngle === false) {
             angleSwitcher(true);
-            return;
-        }
-        if (topAngle === true) {
+        } else {
             angleSwitcher(false);
         }
     })
@@ -212,6 +275,10 @@ function init() {
     let sandboxesMoving = false;
     let topAngle = false;
     let elementMovedDistance = 0;
+    let enableCameraMoving = true;
+    let enableScroll = false;
+    let scrolledUp = false;
+    let isScrolling = false;
 
     function arrowKeyEvent(event) {
         // 判断intro-screen是否关闭
@@ -220,18 +287,42 @@ function init() {
         }
         // 判断之前的移动动画是否结束
         if (sandboxesMoving === true) {
-            return
+            return;
+        }
+        if (cameraMoving === true) {
+            return;
+        }
+        if (isScrolling === true) {
+            return;
         }
 
         let keyCode = event.which;
         if (keyCode === 39) {
             sandboxSwitcher(true);
+            return;
         }
         if (keyCode === 37) {
             sandboxSwitcher(false);
+            return;
+        }
+        // 判断是否启用镜头移动，如果没有，判断是否启用文字滚动，如果没有，则停止识别
+        if (enableCameraMoving === false) {
+            if (enableScroll === true) {
+                if (keyCode === 38) {
+                    scrollPassage(true);
+                    return;
+                }
+                if (keyCode === 40) {
+                    scrollPassage(false);
+                    return;
+                }
+            } else {
+                return;
+            }
         }
         if (keyCode === 38) {
             angleSwitcher(true);
+            return;
         }
         if (keyCode === 40) {
             angleSwitcher(false);
@@ -241,7 +332,7 @@ function init() {
 
     function sandboxSwitcher(direction) {
         // 预设位置
-        let targetPositionsArray = [-30, -15, 0];
+        let targetPositionsArray = [-75, -60, -45, -30, -15, 0];
 
         sandboxesMoving = true;
 
@@ -250,7 +341,7 @@ function init() {
         position.setFromMatrixPosition(sandboxes.matrixWorld);
         let originalPosition = position.x;
 
-        // 当移动前位置在-30
+        // 当移动前位置在-75
         if (originalPosition === targetPositionsArray[0]) {
             if (direction === true) {
                 sandboxesMoving = false;
@@ -296,21 +387,74 @@ function init() {
         function moveX(object, distance) {
             // 背后文字透明度动画
             if (originalPosition === 0) {
-                document.getElementById("passage1").style.color = "rgba(255, 255, 255, 0.3)";
-                document.getElementById("passage2").style.color = "rgba(255, 255, 255, 1)";
+                enableCameraMoving = false;
+                document.getElementById("arrow-up-down-tip").style.color = "rgba(255,255,255,0.3)";
+                document.getElementById("arrow-up-down-shortcut").style.color = "rgba(255,255,255,0.3)";
+
+                document.getElementById("passage1").style.textShadow = "0 0 10px rgba(255, 255, 255, 1)";
+                document.getElementById("passage2").style.textShadow = "0 0 0 rgba(255, 255, 255, 1)";
             }
             if (originalPosition === -15) {
+                enableCameraMoving = true;
+                document.getElementById("arrow-up-down-tip").style.color = "rgba(255,255,255,1)";
+                document.getElementById("arrow-up-down-shortcut").style.color = "rgba(255,255,255,0.6)";
                 if (distance > 0) {
-                    document.getElementById("passage2").style.color = "rgba(255, 255, 255, 0.3)";
-                    document.getElementById("passage1").style.color = "rgba(255, 255, 255, 1)";
+                    document.getElementById("passage2").style.textShadow = "0 0 10px rgba(255, 255, 255, 1)";
+                    document.getElementById("passage1").style.textShadow = "0 0 0 rgba(255, 255, 255, 1)";
                 } else {
-                    document.getElementById("passage2").style.color = "rgba(255, 255, 255, 0.3)";
-                    document.getElementById("passage3").style.color = "rgba(255, 255, 255, 1)";
+                    document.getElementById("passage2").style.textShadow = "0 0 10px rgba(255, 255, 255, 1)";
+                    document.getElementById("passage3").style.textShadow = "0 0 0 rgba(255, 255, 255, 1)";
                 }
             }
             if (originalPosition === -30) {
-                document.getElementById("passage3").style.color = "rgba(255, 255, 255, 0.3)";
-                document.getElementById("passage2").style.color = "rgba(255, 255, 255, 1)";
+                enableCameraMoving = false;
+                document.getElementById("arrow-up-down-tip").style.color = "rgba(255,255,255,0.3)";
+                document.getElementById("arrow-up-down-shortcut").style.color = "rgba(255,255,255,0.3)";
+                if (distance > 0) {
+                    document.getElementById("passage3").style.textShadow = "0 0 10px rgba(255, 255, 255, 1)";
+                    document.getElementById("passage2").style.textShadow = "0 0 0 rgba(255, 255, 255, 1)";
+                } else {
+                    document.getElementById("passage3").style.textShadow = "0 0 10px rgba(255, 255, 255, 1)";
+                    document.getElementById("passage4").style.textShadow = "0 0 0 rgba(255, 255, 255, 1)";
+                }
+            }
+            if (originalPosition === -45) {
+                enableCameraMoving = true;
+                document.getElementById("arrow-up-down-tip").style.color = "rgba(255,255,255,1)";
+                document.getElementById("arrow-up-down-shortcut").style.color = "rgba(255,255,255,0.6)";
+                if (distance > 0) {
+                    document.getElementById("passage4").style.textShadow = "0 0 10px rgba(255, 255, 255, 1)";
+                    document.getElementById("passage3").style.textShadow = "0 0 0 rgba(255, 255, 255, 1)";
+                } else {
+                    document.getElementById("passage4").style.textShadow = "0 0 10px rgba(255, 255, 255, 1)";
+                    document.getElementById("passage5").style.textShadow = "0 0 0 rgba(255, 255, 255, 1)";
+                }
+            }
+            if (originalPosition === -60) {
+                if (distance > 0) {
+                    enableCameraMoving = false;
+                    document.getElementById("arrow-up-down-tip").style.color = "rgba(255,255,255,0.3)";
+                    document.getElementById("arrow-up-down-shortcut").style.color = "rgba(255,255,255,0.3)";
+
+                    document.getElementById("passage5").style.textShadow = "0 0 10px rgba(255, 255, 255, 1)";
+                    document.getElementById("passage4").style.textShadow = "0 0 0 rgba(255, 255, 255, 1)";
+                } else {
+                    enableCameraMoving = false;
+                    enableScroll = true;
+                    document.getElementById("passage5").style.textShadow = "0 0 10px rgba(255, 255, 255, 1)";
+                    document.getElementById("passage6").style.textShadow = "0 0 0 rgba(255, 255, 255, 1)";
+                }
+            }
+            if (originalPosition === -75) {
+                if (scrolledUp === true) {
+                    scrollPassage(false);
+                }
+                enableCameraMoving = true;
+                document.getElementById("arrow-up-down-tip").style.color = "rgba(255,255,255,1)";
+                document.getElementById("arrow-up-down-shortcut").style.color = "rgba(255,255,255,0.6)";
+                enableScroll = false;
+                document.getElementById("passage6").style.textShadow = "0 0 10px rgba(255, 255, 255, 1)";
+                document.getElementById("passage5").style.textShadow = "0 0 0 rgba(255, 255, 255, 1)";
             }
             let movedDistance = 0;  // 初始化移动距离
 
@@ -327,8 +471,12 @@ function init() {
                 }
                 object.translateX(distance / 40);
                 movedDistance += Math.abs(distance / 40);
-                elementMovedDistance += distance / 15;
-                document.getElementById("under-canvas").style.transform = "translate(" + elementMovedDistance + "em)";
+                if (distance > 0) {
+                    elementMovedDistance += 1;
+                } else {
+                    elementMovedDistance -= 1;
+                }
+                document.getElementById("passages").style.transform = "translate(" + elementMovedDistance + "%)";
 
                 renderer.render(scene, camera);
 
@@ -349,9 +497,11 @@ function init() {
                 cameraMoving = false;
                 return;
             }
+
+
             moveCamera(25, 0, 10);
             topAngle = true;
-            document.getElementById("arrow-up-down-tip").textContent = "还原镜头";
+            document.getElementById("arrow-up-down-tip").textContent = "　返回　";
             document.getElementById("arrow-up-down-shortcut").textContent = "↓";
         }
         if (direction === false) {
@@ -362,13 +512,20 @@ function init() {
             }
             moveCamera(0, 25, 10);
             topAngle = false;
-            document.getElementById("arrow-up-down-tip").textContent = "　鸟瞰　";
+            document.getElementById("arrow-up-down-tip").textContent = "详细信息";
             document.getElementById("arrow-up-down-shortcut").textContent = "↑";
         }
 
         function moveCamera(startZ, finishZ, Y) {
             // 初始化移动距离
             let movedDistanceZ = 0;
+            // 初始化文章移动距离
+            let passageDistance = 0;
+            if (startZ > finishZ) {
+                passageDistance = 0;
+            } else {
+                passageDistance = 80;
+            }
 
 
             // 把总距离分成40份作为一步长
@@ -377,10 +534,17 @@ function init() {
 
             window.requestAnimationFrame(moveCameraAction);
 
+            // 镜头慢慢指向sandboxes的高度
+            let lookAtY = 0;
+            if (startZ > finishZ) {
+                lookAtY = 0;
+            } else {
+                lookAtY = -3;
+            }
+
             function moveCameraAction() {
                 // 当移动距离达到目标距离时退出函数
                 if (movedDistanceZ === Math.abs(deltaZ)) {
-
                     cameraMoving = false;
                     movedDistanceZ = 0; // 重置
                     return;
@@ -388,12 +552,72 @@ function init() {
                 camera.translateZ(stepZ);
                 camera.position.y = Y;
                 movedDistanceZ += Math.abs(stepZ);
-                camera.lookAt(0, 0, 0);
 
+                // 调整镜头指向
+                camera.lookAt(0, lookAtY, 0);
+                if (startZ > finishZ) {
+                    lookAtY -= 3 / 40;
+                } else {
+                    lookAtY += 3 / 40;
+                }
+
+                // 调整文章
+                if (startZ > finishZ) {
+                    passageDistance += 2;
+                } else {
+                    passageDistance -= 2;
+                }
+
+                document.getElementById("passages").style.marginTop = passageDistance + "em";
                 renderer.render(scene, camera);
 
                 window.requestAnimationFrame(moveCameraAction);
             }
+        }
+    }
+
+    function scrollPassage(direction) {
+        isScrolling = true;
+        // 初始化文章移动距离
+        let passageDistance = 0;
+        if (direction === true) {
+            passageDistance = 0;
+        } else {
+            passageDistance = -25;
+        }
+
+        window.requestAnimationFrame(scrollY);
+
+        function scrollY() {
+            // 停止条件
+            if (direction === true) {
+                if (passageDistance === -25) {
+                    passageDistance = 0;  // 重置
+                    scrolledUp = true;
+                    document.getElementById("arrow-up-down-tip").textContent = "　返回　";
+                    document.getElementById("arrow-up-down-shortcut").textContent = "↓";
+                    isScrolling = false;
+                    return;
+                }
+            } else {
+                if (passageDistance === 0) {
+                    passageDistance = 0;  // 重置
+                    scrolledUp = false;
+                    document.getElementById("arrow-up-down-tip").textContent = "详细信息";
+                    document.getElementById("arrow-up-down-shortcut").textContent = "↑";
+                    isScrolling = false;
+                    return;
+                }
+            }
+            // 调整文章
+            if (direction === true) {
+                passageDistance -= 1;
+            } else {
+                passageDistance += 1;
+            }
+            document.getElementById("passage6").style.marginTop = passageDistance + "em";
+            renderer.render(scene, camera);
+            window.requestAnimationFrame(scrollY);
         }
     }
 }
